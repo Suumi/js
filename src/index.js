e68fb4500a56d122f65f83eb73391a7928bb200a
Suumi <60427967+Suumi@users.noreply.github.com>
2022-07-22 19:51:57 +0900

    TODO作成途中

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // liタグ生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
